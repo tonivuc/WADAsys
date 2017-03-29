3f280812a78263853f684736dc5b9a450b152867
camilhl <camilhl@stud.ntnu.no>
2017-03-29 10:11:45 +0200

    Added JUnit testing lib

---
+++ Code/login/UserTest.java
@@ -0,0 +1,67 @@
+package login;
+import java.sql.*;
+import DatabaseConnection.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by camhl on 22.03.2017.
+ */
+public class UserTest {
+
+    private User user;
+    private int ADMIN = 0;
+    private int ANALYSOR = 1;
+    private int COLLECTOR = 2;
+
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //user.closeAll();
+
+    }
+
+    @Test
+    public void testLogin() {
+        assertTrue(user.login("Geirmama", "Geirmama123"));
+        assertFalse(user.login("GeirmamA", "Geirmama1235"));
+
+    }
+
+    @Test
+    public void testCheckPassword() {
+        assertTrue(user.login("Geirmama", "Geirmama123"));
+        assertFalse(user.login("Geirmama", "Geirmama1235"));
+
+
+    }
+
+    @Test
+    public void testFindUsertype() throws Exception {
+        int actualUsertype = user.findUsertype("Geirmama");
+        int expectedUserType = ADMIN;
+        int notExpectedUserType = COLLECTOR;
+
+        assertEquals(expectedUserType, actualUsertype);
+        assertNotSame(notExpectedUserType, actualUsertype);
+
+
+    }
+
+    @Test
+    public void testFindUsertypeByIndex(){
+        assertEquals(user.findUserByIndex(0).toUpperCase(), ADMIN);
+        assertEquals(user.findUserByIndex(2).toUpperCase(), COLLECTOR);
+
+    }
+}
