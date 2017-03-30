5065ad7b42f06e187f745c2a7b80043fee64b604
camilhl <camilhl@stud.ntnu.no>
2017-03-30 11:15:32 +0200

    Added (edituser and deleteuser) in User

--- Code/login/UserTest.java
+++ Code/login/UserTest.java
@@ -2,67 +2,83 @@ package login;
 import java.sql.*;
 import DatabaseConnection.*;
 
-/*import org.junit.*;
+import org.junit.*;
 import org.junit.runners.JUnit4;
 
 import javax.swing.plaf.nimbus.State;
 import javax.xml.crypto.Data;
 
-import static org.junit.Assert.*;*/
+import static org.junit.Assert.*;
 
 /**
  * Created by camhl on 22.03.2017.
  */
-/*public class UserTest {
-
+public class UserTest{
     private User user;
     private int ADMIN = 0;
     private int ANALYSOR = 1;
     private int COLLECTOR = 2;
 
     @Before
-    public void setUp() throws Exception {
-
-        user = new User();
+    public void start() throws Exception {
+        this.user = new User();
+        user.setup();
     }
 
     @After
     public void tearDown() throws Exception {
-        //user.closeAll();
-
+        user.disconnect();
     }
 
     @Test
-    public void testLogin() {
+    public void login() throws Exception {
         assertTrue(user.login("Geirmama", "Geirmama123"));
         assertFalse(user.login("GeirmamA", "Geirmama1235"));
-
     }
 
     @Test
-    public void testCheckPassword() {
+    public void checkPassword() throws Exception {
+
         assertTrue(user.login("Geirmama", "Geirmama123"));
         assertFalse(user.login("Geirmama", "Geirmama1235"));
 
-
     }
 
     @Test
-    public void testFindUsertype() throws Exception {
+    public void findUser() throws Exception {
+        assertTrue(user.findUser("Geirmama"));
+        assertFalse(user.findUser("Geirmama321"));
+    }
+
+    @Test
+    public void findUsertype() throws Exception {
+
         int actualUsertype = user.findUsertype("Geirmama");
         int expectedUserType = ADMIN;
         int notExpectedUserType = COLLECTOR;
+        assertEquals((long) expectedUserType, (long) actualUsertype);
+        assertNotSame(Integer.valueOf(notExpectedUserType), Integer.valueOf(actualUsertype));
 
-        assertEquals(expectedUserType, actualUsertype);
-        assertNotSame(notExpectedUserType, actualUsertype);
+    }
+
+    @Test
+    public void findUserByIndex() throws Exception {
+
+        assertEquals(user.findUserByIndex(0).toUpperCase(), "ADMIN");
+        assertEquals(user.findUserByIndex(1).toUpperCase(), "ANALYST");
+        assertEquals(user.findUserByIndex(2).toUpperCase(), "COLLECTOR");
+
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+
 
 
     }
 
     @Test
-    public void testFindUsertypeByIndex(){
-        assertEquals(user.findUserByIndex(0).toUpperCase(), ADMIN);
-        //assertEquals(user.findUserByIndex(2).toUpperCase(), COLLECTOR);
+    public void editName() throws Exception {
 
     }
-}*/
+}
