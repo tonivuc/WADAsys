36ca53e7159d567f36621b1bc25223b896ab6c7a
camilhl <camilhl@stud.ntnu.no>
2017-04-04 17:59:29 +0200

    Merge branch 'master' of https://gitlab.stud.iie.ntnu.no/toniv/WADA_sys
    # Conflicts:
    #	Code/backend/Athlete.java

--- Code/GUI/admin/deleteAdminUser.java
+++ Code/GUI/admin/deleteAdminUser.java
@@ -1,37 +1,19 @@
-package admin;
-import backend.Athlete;
+package GUI.admin;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 
-import static com.sun.glass.ui.Cursor.setVisible;
-
-public class deleteAdminUser extends JFrame{
-    private JButton addUserButton;
-    private JButton editUserButton;
-    private JButton deleteUserButton;
-    private JButton logOutButton;
-    private JTextField searchTextField;
-    private JPanel rootpanel;
-    private JButton searchButton;
-    private JButton deleteButton;
-    private JScrollPane AthleteList;
-    private JPanel MidPanel;
+public class deleteAdminUser {
+    JPanel panel1;
+    JList list1;
+    JButton button1;
+    JTextField textField1;
 
     public deleteAdminUser() {
 
-    searchButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+    }
 
-        }
-    });
-
+    public JPanel getMainPanel(){
+        return panel1;
     }
 
     public static void main(String[] args){
@@ -40,7 +22,7 @@ public class deleteAdminUser extends JFrame{
         window.setVisible(true);*/
 
         JFrame frame = new JFrame("Add user"); //Creating JFrame
-        frame.setContentPane(new deleteAdminUser().rootpanel); //Setting content pane to rootPanel, which shows the window allowing the administrator to add user
+        frame.setContentPane(new deleteAdminUser().panel1); //Setting content pane to rootPanel, which shows the window allowing the administrator to add user
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The window will close if you press exit
         frame.pack();  //Creates a window out of all the components
         frame.setVisible(true);   //Setting the window visible
