523b5af78e96fcd7d0fdfd2992288262643a095f
trymvg <trymvg@stud.ntnu.no>
2017-04-05 09:19:41 +0200

    Watchlist fungerer nå

--- Code/GUI/analyst/WatchlistPanel.java
+++ Code/GUI/analyst/WatchlistPanel.java
@@ -1,6 +1,5 @@
 package GUI.analyst;
 
-import backend.Athlete;
 import backend.Watchlist;
 import backend.WatchlistTableModel;
 
@@ -8,7 +7,6 @@ import javax.swing.*;
 import javax.swing.table.TableModel;
 import java.awt.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,36 +21,35 @@ public class WatchlistPanel extends JPanel {
     private LocalDate date;
     private Watchlist watchlist;
     private List listAthletes;
+    TableModel tableModel;
 
     public WatchlistPanel(LocalDate date) {
+
+        mainPanel.setLayout(new BorderLayout());
 
         this.date = date;
         this.watchlist = new Watchlist();
 
-
         this.listAthletes = watchlist.getSuspiciousAthletes(date);
-        TableModel tableModel = new WatchlistTableModel(listAthletes);
+        tableModel = new WatchlistTableModel(listAthletes);
         athleteTable = new JTable(tableModel);
 
+        tableScrollPane = new JScrollPane(athleteTable);
+        mainPanel.add(tableScrollPane, BorderLayout.CENTER);
+
+
         athleteTable.setAutoCreateRowSorter(true);
-
-        add(new JScrollPane(athleteTable), BorderLayout.CENTER);
-
-        /*
-        YO
-        pack();
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        */
-    }
-
-    public List<Athlete> createListAthlete() {
-        List<Athlete> listAthlete = new ArrayList<>();
-
-        return listAthlete;
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public static void main(String[]args){
+        JFrame frame = new JFrame("Watchlist"); //Creating JFrame
+        frame.setContentPane(new WatchlistPanel(LocalDate.of(2017, 04, 10)).mainPanel); //Setting content pane to rootPanel, which shows the window allowing the administrator to add user
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The window will close if you press exit
+        frame.pack();  //Creates a window out of all the components
+        frame.setVisible(true);   //Setting the window visible
     }
 }
