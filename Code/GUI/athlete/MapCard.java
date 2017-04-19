217b66538bce76c79b51ffd1cebedd633b2bb01b
camilhl <camilhl@stud.ntnu.no>
2017-04-19 15:05:53 +0200

    Merge branch 'master' of https://gitlab.stud.iie.ntnu.no/toniv/WADA_sys
    # Conflicts:
    #	Code/backend/Athlete.java

--- Code/GUI/athlete/MapCard.java
+++ Code/GUI/athlete/MapCard.java
@@ -10,15 +10,15 @@ import javax.swing.*;
 /**
  * Created by camhl on 19.04.2017.
  */
-public class MapCard extends JFrame{
+public class MapCard extends JPanel{
     private JPanel rootPanel;
 
     public MapCard(String latitude, String longitude){
 
         Map map = new Map();
         add(new Map().getMap(latitude, longitude));
-        pack();
-        setVisible(true);
+        //pack();
+        //setVisible(true);
     }
 
     public JPanel getMainPanel(){
