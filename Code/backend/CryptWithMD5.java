7acc09fe974c97d3a5f012f52ea923c1c9995292
camilhl <camilhl@stud.ntnu.no>
2017-04-19 10:44:29 +0200

    Merge branch 'master' of https://gitlab.stud.iie.ntnu.no/toniv/WADA_sys
    # Conflicts:
    #	Code/backend/Athlete.java

---
+++ Code/backend/CryptWithMD5.java
@@ -0,0 +1,37 @@
+package backend;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by camhl on 19.04.2017.
+ */
+
+public class CryptWithMD5 {
+    private static MessageDigest md;
+
+    public static String cryptWithMD5(String pass){
+        try {
+            md = MessageDigest.getInstance("MD5");
+            byte[] passBytes = pass.getBytes();
+            md.reset();
+            byte[] digested = md.digest(passBytes);
+            StringBuffer sb = new StringBuffer();
+            for(int i=0;i<digested.length;i++){
+                sb.append(Integer.toHexString(0xff & digested[i]));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(CryptWithMD5.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+
+
+    }
+
+    public static void main(String[]args){
+        System.out.println(cryptWithMD5("Geirmama123"));
+    }
+}
