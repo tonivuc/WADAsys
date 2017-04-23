b4e1566654588ad36bed895316f97117f398c6d8
trymvg <trymvg@stud.ntnu.no>
2017-04-23 17:33:10 +0200

    LocationAdder nesten ferdig. Den finner ikke automatisk høyde ut ifra lokasjon

--- Code/GUI/login/LoginWindow.java
+++ Code/GUI/login/LoginWindow.java
@@ -2,6 +2,8 @@ package GUI.login;
 
 import GUI.BaseWindow;
 import GUI.main.MainWindow;
+import backend.CSVReader;
+import backend.LocationAdder;
 import backend.RandomPasswordGenerator;
 import backend.User;
 
@@ -12,6 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.util.ArrayList;
 
 import static backend.SendEmail.sendPasswordToUser;
 import static javax.swing.JOptionPane.showInputDialog;
@@ -280,6 +283,12 @@ public class LoginWindow extends BaseWindow implements ActionListener {
         if (testUser.login(username, password)) {
             loggedin = true;
             loginType = testUser.findUsertype(username);
+
+            //Adds locations from the CSV-file into the database before logging in
+            CSVReader csvReader = new CSVReader();
+            ArrayList<String[]> locationList = csvReader.getCSVContent();
+            LocationAdder la = new LocationAdder();
+            la.addLocations(locationList);
 
             System.out.println("Login Ok!");
         } else {
--- Code/backend/CSVReader.java
+++ Code/backend/CSVReader.java
@@ -7,35 +7,57 @@ package backend;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVReader {
 
+    private String csvFile = "/Users/tvg-b/documents/NTNU/SYS/WADASYSNEW/WADA_sys/CSV_files/locations.csv";
+    private String line = "";
+    private String CSVSPLITBY = ";";
 
+    public CSVReader (String splitCSVby, String csvFileLocation) {
+        this.csvFile = csvFileLocation;
+        this.CSVSPLITBY = splitCSVby;
+    }
 
     public CSVReader () {
 
     }
 
-    public static void main(String[] args) {
+    public ArrayList<String[]> getCSVContent () {
 
-        String csvFile = "/Users/tvg-b/documents/NTNU/SYS/WADASYSNEW/WADA_sys/CSV_files/locations20170422.csv";
-        String line = "";
-        String cvsSplitBy = ";";
+        ArrayList<String[]> stringList = new ArrayList<String[]>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
             while ((line = br.readLine()) != null) {
 
-                // use semicolon as separator
-                String[] location = line.split(cvsSplitBy);
-
-                System.out.println("Name: " + location[0] + " , location: " + location[1] + ", fromDate: " + location[2] + ", toDate: " + location[3]);
-
+                String[] location = line.split(CSVSPLITBY);
+                stringList.add(location);
 
             }
 
+            return stringList;
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("IOException method getCSVContent in class CSVReader.java: " + e );
+        }
+
+        return null;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        CSVReader csvReader = new CSVReader();
+        ArrayList locationList = csvReader.getCSVContent();
+
+        for (int i = 0; i < locationList.size(); i++) {
+
+            String[] newLocation = (String[]) locationList.get(i);
+
+            System.out.println(newLocation[0] + ", " + newLocation[1] + ", " + newLocation[2] + ", " + newLocation[3] + ", " + newLocation[4]);
+
         }
 
     }
