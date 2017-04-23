017de2fbbacfed98a9efa636ffe61826599fead6
Toni Vucic <toniv@stud.ntnu.no>
2017-04-23 17:27:53 +0200

    BIG COMMIT: Deleted EditUser and integrated functionality into EditUser Made changes to all Search windows to remove NullPointerException bug Made BaseWindowAdmin work

---
+++ Code/GUI/testWindows/testJPanel.java
@@ -0,0 +1,31 @@
+package GUI.testWindows;
+
+import GUI.BaseWindow;
+
+import javax.swing.*;
+
+/**
+ * Created by Toni on 20.04.2017.
+ */
+public class testJPanel extends JPanel{
+    private JLabel hiLabel;
+    private JTextField mainTextfield;
+    private JPanel mainPanel;
+    private int niceInt = 0;
+
+    testJPanel() {
+        add(mainPanel);
+        niceInt = 2;
+    }
+
+    public int getNiceInt() {
+        return niceInt;
+    }
+
+    public static void main(String[] args) {
+        BaseWindow window = new BaseWindow();
+        window.add(new testJPanel());
+        window.pack();
+        window.setVisible(true);
+    }
+}
