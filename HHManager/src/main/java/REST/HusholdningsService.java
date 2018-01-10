d8f37741ffe1073c67f5f10e4acac090380b89bf
tonivuc <tonivuc@gmail.com>
2018-01-10 10:07:21 +0100

    Startet metode for å lage ny husholdning

--- HHManager/src/main/java/REST/HusholdningsService.java
+++ HHManager/src/main/java/REST/HusholdningsService.java
@@ -1,5 +1,19 @@
 package REST;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+
+@Path("/HHService")
 public class HusholdningsService {
+
+    @POST
+    @Path("/husholdning/{navn}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public int lagreNyHusholdning(@PathParam("quizId") int quizId) {
+        //Sende husholdningsnavn til database
+        //for å lagre ny Husholdning. SQL lager ny ID, vi returnerer den
+        int husholdningsId = 1337;
+        return husholdningsId;
+    }
 
 }
