b3a03825465c97bc76d7529a5f3bc7c275ac5575
Brogztato <bragehs@hotmail.com>
2018-01-10 09:29:10 +0100

    brage, java

---
+++ HHManager/src/main/java/REST/Handleliste.java
@@ -0,0 +1,22 @@
+package REST;
+
+/**
+ * Created by BrageHalse on 10.01.2018.
+ */
+public class Handleliste {
+    private int handlelisteId;
+    private int husholdningsId;
+    private int skaperId;
+
+    public int getHandlelisteId(){
+        return handlelisteId;
+    }
+
+    public int getHusholdningsId(){
+        return husholdningsId;
+    }
+
+    public int getSkaperId(){
+        return skaperId;
+    }
+}
