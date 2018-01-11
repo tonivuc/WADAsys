0d0fdb135023465eed34cf372e4ec84d60364286
tonivuc <tonivuc@gmail.com>
2018-01-11 13:24:56 +0100

    Splittet opp "Database"-klassen og dro ut eksempelmetoden

--- HHManager/src/main/java/server/controllers/LoginController.java
+++ HHManager/src/main/java/server/controllers/LoginController.java
@@ -3,6 +3,31 @@ package server.controllers;
 // Vi har controller-klasser fordi da kan vi teste login-funksjonalitet uten å bruke services. JUnit og sånn.
 // Her kan vi også ha SQL-kall
 
+import server.database.Database;
+import server.restklasser.Bruker;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginController {
-    
+
+    private static final String SQL_EXIST = "SELECT * FROM bruker WHERE brukerId=? AND passord=?";
+
+    public boolean exist(Bruker user) throws SQLException {
+        boolean exist = false;
+
+        try (Connection connection = Database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_EXIST);) {
+            statement.setString(1, Integer.toString(user.getBrukerId()));
+            statement.setString(2, user.getPassord());
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                exist = resultSet.next();
+            }
+        }
+
+        return exist;
+    }
 }
--- HHManager/src/main/java/server/database/Database.java
+++ HHManager/src/main/java/server/database/Database.java
@@ -1,10 +1,9 @@
 package server.database;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import server.controllers.LoginController;
 import server.restklasser.Bruker;
 
-import javax.servlet.ServletException;
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,34 +28,17 @@ public final class Database {
         return dataSource.getConnection();
     }
 
-    private static final String SQL_EXIST = "SELECT * FROM bruker WHERE brukerId=? AND passord=?";
-
-    public boolean exist(Bruker user) throws SQLException {
-        boolean exist = false;
-
-        try (Connection connection = Database.getConnection();
-                PreparedStatement statement = connection.prepareStatement(SQL_EXIST);) {
-            statement.setString(1, Integer.toString(user.getBrukerId()));
-            statement.setString(2, user.getPassord());
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                exist = resultSet.next();
-            }
-        }
-
-        return exist;
-    }
-
     public static void main(String[] args) {
 
         Database testDatabase = new Database();
         Bruker testBruker = new Bruker();
+        LoginController controller = new LoginController();
         testBruker.setBrukerId(1);
         testBruker.setPassord("passord1");
 
 
         try {
-            if (!testDatabase.exist(testBruker)) {
+            if (!controller.exist(testBruker)) {
                 System.out.println("Bruker finnes ikke");
             } else {
                 System.out.println("Brukeren finnes!");
