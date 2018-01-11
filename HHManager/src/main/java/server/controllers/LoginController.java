aa5751a594dd7492aee21b7cb63805c657954969
tonivuc <tonivuc@gmail.com>
2018-01-11 09:38:15 +0100

    Tester xml endring

---
+++ HHManager/src/main/java/server/controllers/LoginController.java
@@ -0,0 +1,8 @@
+package server.controllers;
+
+// Vi har controller-klasser fordi da kan vi teste login-funksjonalitet uten å bruke services. JUnit og sånn.
+// Her kan vi også ha SQL-kall
+
+public class LoginController {
+    
+}
