b6d7fb271d4fd8c8b70aae5936ba2a2f731d3744
trulsmt <trulsmt@stud.ntnu.no>
2018-01-11 12:47:15 +0100

    skal merge med tonidbtest

--- HHManager/src/main/java/server/Rest.java
+++ HHManager/src/main/java/server/Rest.java
@@ -7,28 +7,42 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Collection;
+
 /**
  * Created by Hallvard on 08.01.2018.
  */
 @Path("/brukere/")
 public class Rest {
-
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<String> getBrukere() {
+    public ArrayList<ArrayList<String>> getBrukere() {
         System.out.println("Getter");
+        ArrayList<ArrayList<String>> arrayList = new ArrayList<>();
         try (Result result = DatabaseConnection.fetchData("Select * from bruker", new String[0])) {
             ResultSet resultSet = result.getResultSet();
-            while (resultSet.next()) {
+            int ant = resultSet.getRow();
+            for (int i = 0; i < ant; i++) {
                 System.out.println(resultSet.next());
+                arrayList.add((java.util.ArrayList) resultSet.getArray(i));
+                i++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("forste element");
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        arrayList2.add("forste element");
+        arrayList.add(arrayList2);
         return arrayList;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        try(Result result = DatabaseConnection.fetchData("Select * from bruker", new String[0])){
+            ResultSet resultSet = result.getResultSet();
+            System.out.println(resultSet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
